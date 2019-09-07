40883a1c813f3b9bc0c6240ec03004e9c3f671ef
Jimmy <jimmych679@gmail.com>
2019-09-06 21:34:40 -0400

    removed reload button and calculate score button and replaced its funtionality

--- GUIversion.py
+++ GUIversion.py
@@ -16,7 +16,7 @@ class ChampionTool(tk.Tk):
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
         self.frames = {}
-        for F in (ChampionPage, Championselectionpage, recommandationpage):
+        for F in (recommandationpage,ChampionPage, Championselectionpage):
             frame = F(container, self)
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky="nsew")
@@ -80,13 +80,13 @@ class ChampionPage(tk.Frame):
         echamp_3.grid(row=2, column=2, padx=50, pady=50)
         echamp_4.grid(row=3, column=2, padx=50, pady=50)
         echamp_5.grid(row=4, column=2, padx=50, pady=50)
-        reloadbutton = tk.Button(self, text="reload", command=self.get_champ)
 
         poolbutton = tk.Button(self, text="MainChamp",command=lambda:[self.specific_position(0, 'm'),
                                                                       controller.show_frame(Championselectionpage)])
         poolbutton.grid(row=4, padx=50, pady=50)
-        reloadbutton.grid(row=5, column=2, padx=50, pady=50)
-        resultbutton = tk.Button(self, text="RESULT", command=lambda: controller.show_frame(recommandationpage))
+        CPP = self.controller.get_page(recommandationpage)
+        resultbutton = tk.Button(self, text="RESULT" , command=lambda: [recommandationpage.outputrecommanded(CPP),
+                                                                     controller.show_frame(recommandationpage)])
         resultbutton.grid(row=6, column =2, padx=50, pady=50)
 
     def get_champ(self):
@@ -212,17 +212,19 @@ class recommandationpage(tk.Frame):
     def __init__(self, parent, controller):
         self.controller = controller
         self.final_list = []
-        CP = self.controller.get_page(ChampionPage)
-        tk.Frame.__init__(self, parent)
-        calb = tk.Button(self, text="Calculate Result", command=lambda: self.outputrecommanded(CP))
-        calb.grid(row = 0)
 
-    def outputrecommanded(self, cp):
+        tk.Frame.__init__(self, parent)
+        Endbutton =tk.Button(self, text="End Program",command=lambda: self.quit())
+        Endbutton.grid(row = 5)
+
+    def outputrecommanded(self):
+        cp = self.controller.get_page(ChampionPage)
         CG = cp.CG
         CL = cp.CL
         user = cp.user
         SC = Scorecalculator(CL, CG, user)
         att_list = SC.att_in_need()
+        print(att_list)
         SC.calculate_champscore(att_list)
         SAT = ScoreAVLTree()
         TopNode = None
@@ -238,7 +240,7 @@ class recommandationpage(tk.Frame):
             champion = tk.Label(self, image=photo)
             champion.image= photo
             champion.grid(row= position, column= 1)
-            position+=1
+            position += 1
 
 
 class Championselectionpage(tk.Frame):
@@ -247,6 +249,7 @@ class Championselectionpage(tk.Frame):
         self.controller = controller
         self.champ = None
         tk.Frame.__init__(self, parent)
+        CP = self.controller.get_page(ChampionPage)
         photo1 = tk.PhotoImage(file=r"C:\Users\Jimmy\PycharmProjects\leaguetool\character pic\AatroxSquare.png")
         photo2 = tk.PhotoImage(file=r"C:\Users\Jimmy\PycharmProjects\leaguetool\character pic\AhriSquare.png")
         photo3 = tk.PhotoImage(file=r"C:\Users\Jimmy\PycharmProjects\leaguetool\character pic\AkaliSquare.png")
@@ -263,20 +266,20 @@ class Championselectionpage(tk.Frame):
         photo14 = tk.PhotoImage(file=r"C:\Users\Jimmy\PycharmProjects\leaguetool\character pic\BraumSquare.png")
         canvas = tk.Canvas(self, width=160, height=160)
         canvas.grid(row=1)
-        Aatroxbutton = tk.Button(self, image=photo1, command=self.return_Aatrox)
-        ahributton = tk.Button(self, image=photo2, command=self.return_Ahri)
-        Akalibutton = tk.Button(self, image=photo3, command=self.return_Akali)
-        Alistarbutton = tk.Button(self, image=photo4, command=self.return_Alistar)
-        Amumubutton = tk.Button(self, image=photo5, command=self.return_Amumu)
-        Aniviabutton = tk.Button(self, image=photo6, command=self.return_Anivia)
-        Anniebutton = tk.Button(self, image=photo7, command=self.return_Annie)
-        ashebutton = tk.Button(self, image=photo8, command=self.return_Ashe)
-        Aurelion_Solbutton = tk.Button(self, image=photo9, command=self.return_Aurelion)
-        Azirbutton = tk.Button(self, image=photo10, command=self.return_Azir)
-        Bardbutton = tk.Button(self, image=photo11, command=self.return_Bard)
-        Blitzcrankbutton = tk.Button(self, image=photo12, command=self.return_Blitzcrank)
-        Brandbutton = tk.Button(self, image=photo13, command=self.return_Brand)
-        Braumbutton = tk.Button(self, image=photo14, command=self.return_Braum)
+        Aatroxbutton = tk.Button(self, image=photo1, command=lambda: [self.return_Aatrox(), ChampionPage.get_champ(CP), controller.show_frame(ChampionPage)])
+        ahributton = tk.Button(self, image=photo2, command=lambda: [self.return_Ahri(), ChampionPage.get_champ(CP), controller.show_frame(ChampionPage)])
+        Akalibutton = tk.Button(self, image=photo3, command=lambda: [self.return_Akali(), ChampionPage.get_champ(CP), controller.show_frame(ChampionPage)])
+        Alistarbutton = tk.Button(self, image=photo4, command=lambda: [self.return_Alistar(), ChampionPage.get_champ(CP), controller.show_frame(ChampionPage)])
+        Amumubutton = tk.Button(self, image=photo5, command=lambda: [self.return_Amumu(), ChampionPage.get_champ(CP), controller.show_frame(ChampionPage)])
+        Aniviabutton = tk.Button(self, image=photo6, command=lambda: [self.return_Anivia(), ChampionPage.get_champ(CP), controller.show_frame(ChampionPage)])
+        Anniebutton = tk.Button(self, image=photo7, command=lambda: [self.return_Annie(), ChampionPage.get_champ(CP), controller.show_frame(ChampionPage)])
+        ashebutton = tk.Button(self, image=photo8, command=lambda: [self.return_Ashe(), ChampionPage.get_champ(CP), controller.show_frame(ChampionPage)])
+        Aurelion_Solbutton = tk.Button(self, image=photo9, command=lambda: [self.return_Aurelion(), ChampionPage.get_champ(CP), controller.show_frame(ChampionPage)])
+        Azirbutton = tk.Button(self, image=photo10, command=lambda: [self.return_Azir(), ChampionPage.get_champ(CP), controller.show_frame(ChampionPage)])
+        Bardbutton = tk.Button(self, image=photo11, command=lambda: [self.return_Bard(), ChampionPage.get_champ(CP), controller.show_frame(ChampionPage)])
+        Blitzcrankbutton = tk.Button(self, image=photo12, command=lambda: [self.return_Blitzcrank(), ChampionPage.get_champ(CP), controller.show_frame(ChampionPage)])
+        Brandbutton = tk.Button(self, image=photo13, command=lambda: [self.return_Brand(), ChampionPage.get_champ(CP), controller.show_frame(ChampionPage)])
+        Braumbutton = tk.Button(self, image=photo14, command=lambda: [self.return_Braum(), ChampionPage.get_champ(CP), controller.show_frame(ChampionPage)])
         Aatroxbutton.image = photo1
         ahributton.image = photo2
         Akalibutton.image = photo3
@@ -306,8 +309,6 @@ class Championselectionpage(tk.Frame):
         Brandbutton.grid(row=3, column=3)
         Braumbutton.grid(row=3, column=4)
         canvas.grid()
-        backbutton = tk.Button(self, text="Back to Main", command=lambda: controller.show_frame(ChampionPage))
-        backbutton.grid(row=4, column= 1, pady=50)
 
     def return_Aatrox(self):
         self.champ = "Aatrox"
